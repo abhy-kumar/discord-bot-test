714e805d21b726a7d162eb8ec97a8ec8b0e669b0
Abhishek Kumar <abhiks177@gmail.com>
2019-05-25 12:29:36 +0530

    Working Code Added

--- test.py
+++ test.py
@@ -1,9 +1,24 @@
 import discord
 
 client = discord.Client()
+prefix = "&"
+
 
 @client.event
 async def on_ready():
-    print("The bot is ready!")
-    await client.change_presence(game=discord.Game(name="Working on itself"))
-    
+    print("Trimmau is ready for your orders.")
+    await client.change_presence(activity=discord.Game("Working for El-Melloi"))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('&hello'):
+        await message.channel.send('Hello. How may I serve you?')
+
+    if message.content.startswith('&sub') or message.content.startswith('&drop'):
+        await message.channel.send('https://docs.google.com/spreadsheets/d/1TgpuozjukEdkOJdGGn-cWD3sIzMWCqnzeaycJsvmjTk/edit#gid=495949767')
+
+
+client.run('NTgxMzY2Mjc0NDc1NTU2ODc0.XOi7vg.SNwHGRSzeCsYroLKS8IkpBX6Gv4')
